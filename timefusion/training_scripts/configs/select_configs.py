2a0f9c8cd8d2c55c0a175da0afd2d13c967da184
Edvard Holen <ejh19@cx3-8-12.cx3.hpc.ic.ac.uk>
2023-05-30 02:07:42 +0100

    merging from jupyterlabs

--- timefusion/training_scripts/configs/select_configs.py
+++ timefusion/training_scripts/configs/select_configs.py
@@ -4,7 +4,7 @@ import numpy as np
 from typing import Dict, Any
 
 num_trials = 50
-random_seed = 5
+random_seed = 15
 config_name = f"electricity_{random_seed}"
 
 def get_hyperparameters(ranges: Dict[str,Any], parameter_id: int) -> Dict[str,Any]:
